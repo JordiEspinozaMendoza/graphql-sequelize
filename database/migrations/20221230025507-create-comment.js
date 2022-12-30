f9966a97bed32ce62f9c9f84b6d40b620c5d2e28
Jordi <jordi8101@gmail.com>
2022-12-30 09:45:22 -0800

    Added models

---
+++ database/migrations/20221230025507-create-comment.js
@@ -0,0 +1,40 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Comments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        type: Sequelize.TEXT,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+      },
+      postId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Posts",
+          },
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Comments");
+  },
+};
