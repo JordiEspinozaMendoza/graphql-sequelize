bfe108c37778a1e38daddddbb4ac397103a8d51a
Jordi <jordi8101@gmail.com>
2022-12-30 16:40:06 -0800

    Schemas

---
+++ graphql/schemas/comment.js
@@ -0,0 +1,21 @@
+// graphql/schemas/comment.js
+
+const { gql } = require("apollo-server-express");
+
+module.exports = gql`
+  type Comment {
+    id: Int!
+    content: String!
+    author: User!
+    post: Post!
+    createdAt: String
+  }
+  extend type Mutation {
+    createComment(content: String!, postId: Int!): CreateCommentResponse
+  }
+  type CreateCommentResponse {
+    id: Int!
+    content: String!
+    createdAt: String
+  }
+`;
--- graphql/schemas/index.js
+++ graphql/schemas/index.js
@@ -1,13 +1,15 @@
 // graphql/schemas/index.js
 const { gql } = require("apollo-server-express");
-
+const userType = require("./user");
+const postType = require("./post");
+const commentType = require("./comment");
 const rootType = gql`
-    type Query {
-        root: String
-    }
-    type Mutation {
-        root: String
-    }
+  type Query {
+    root: String
+  }
+  type Mutation {
+    root: String
+  }
 `;
 
-module.exports = [rootType];
+module.exports = [rootType, userType, postType, commentType];
