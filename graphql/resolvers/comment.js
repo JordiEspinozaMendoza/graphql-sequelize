6f7a6fbb62c1f321ce9c6e1827ab4c42e6f20e6d
Jordi <jordi8101@gmail.com>
2022-12-30 16:39:50 -0800

    Added resolvers

---
+++ graphql/resolvers/comment.js
@@ -0,0 +1,33 @@
+// graphql/resolvers/comment.js
+
+const { Post } = require("../../database/models");
+
+const { AuthenticationError, ApolloError } = require("apollo-server-express");
+
+module.exports = {
+  Mutation: {
+    async createComment(_, { content, postId }, { user = null }) {
+      if (!user) {
+        throw new AuthenticationError(
+          "You must be logged in to create a comment"
+        );
+      }
+      const post = await Post.findByPk(postId);
+      if (post) {
+        return post.createComment({
+          content,
+          userId: user.id,
+        });
+      }
+      throw new ApolloError("Unable to create a comment");
+    },
+  },
+  Comment: {
+    author(comment) {
+      return comment.getAuthor();
+    },
+    post(comment) {
+      return comment.getPost();
+    },
+  },
+};
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,3 +1,7 @@
 // graphql/resolvers/index.js
 
-module.exports = [];
+const userResolvers = require("./user");
+const postResolvers = require("./post");
+const commentResolvers = require("./comment");
+
+module.exports = [userResolvers, postResolvers, commentResolvers];
